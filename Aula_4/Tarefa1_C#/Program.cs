002852bf03a3e21e2d0a224b0d9ba0e460e598a2
NickAdrian0 <nicolaspadriano@gmail.com>
2025-03-17 11:39:45 -0300

    Commit Tarefa Aula4
    Exercício 1 em C#

---
+++ Aula_4/Tarefa1_C#/Program.cs
@@ -0,0 +1,6 @@
+﻿int X;
+
+Console.WriteLine("Bem vindo a calculadora de numeros pares. \nInsira o seu numero e eu lhe direi se ele é par ou impar");
+X = int.Parse(Console.ReadLine());
+if (X % 2 == 0) Console.WriteLine("Seu numero eh par");
+else Console.WriteLine("Seu numero eh impar");
