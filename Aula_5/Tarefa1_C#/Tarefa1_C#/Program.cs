e0171887cae25b6533fe7da2e8b626c4886757b4
NickAdrian0 <nicolaspadriano@gmail.com>
2025-03-21 09:17:18 -0300

    Tarefa Aula 5 Tarefa 1
    Tarefa da Aula 5,Tarefa 1, em C#

---
+++ Aula_5/Tarefa1_C#/Tarefa1_C#/Program.cs
@@ -0,0 +1,12 @@
+﻿int qnum, num, total = 0;
+Console.WriteLine("Bem vindo a calculadora de media aritimetica de numeros pares.\nPor favor, insira a quantidade de numeros que deseja.");
+qnum = int.Parse(Console.ReadLine());
+if (qnum != 0) {
+Console.WriteLine("Agora, digite os numeros que deseja fazer a media");
+for (int i = 0; i < qnum; i++)
+{
+    num = int.Parse(Console.ReadLine());
+    if (num % 2 == 0) total += num;
+} 
+Console.WriteLine($"A soma total dos numeros pares inseridos é de {total}");    
+} else Console.WriteLine("O numero inserido eh invalido, reinicie o programa e tente novamente.");
