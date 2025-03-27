91f4fcf6667852ce2cd06c96d8cec487dcff93e6
NickAdrian0 <nicolaspadriano@gmail.com>
2025-03-26 23:11:46 -0300

    Aula 5 Tarefa 3

---
+++ Aula_5/Tarefa3_C#/Tarefa3_C#/Program.cs
@@ -0,0 +1,8 @@
+﻿        int sum = 0;
+        Console.WriteLine("Bem vindo a calculdora de numeros impares multiplos de 3 entre 50 a 500\nPara confirmar o uso do programa, pressione a tecla 'Y' e confirme");
+        string confirm = Console.ReadLine();
+        if (confirm == "Y") {
+            for (int i = 50; i <= 500; i++) if (i % 3 == 0 && i % 2 != 0) sum += i;
+            Console.WriteLine($"A soma dos números impares multiplos de 3 entre 50 a 500 equivale a: {sum}\n");
+        } else Console.WriteLine("A tecla 'Y' nao foi inserida corretamete. Tente novamente\n");
+        Console.ReadLine();
