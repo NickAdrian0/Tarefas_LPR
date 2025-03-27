27409f2c7814a30a3ceaa8e1ad4f5dbf9b0f0b22
NickAdrian0 <nicolaspadriano@gmail.com>
2025-03-27 03:03:03 -0300

    Aula 5 Tarefa 4

---
+++ Aula_5/Tarefa4_C#/Tarefa4_C#/Program.cs
@@ -0,0 +1,8 @@
+﻿        int num, soma = 0;
+        Console.Write("Ola, seja bem vindo a calculadora de soma de digitos de um quadradro numerico\nIrei elevar o numero inserido, e somar seus caractres\nPor favor, insira o numero desejado:\n");
+        num = int.Parse(Console.ReadLine());
+        num *= num;
+        string sNum = num.ToString();
+        foreach (char digit in sNum) soma += int.Parse(digit.ToString());
+        Console.WriteLine($"A soma dos dígitos de {num} elevado ao quadrado equivale a {soma}");
+        Console.ReadLine();
